20d72b6fab0e5f7ddcd68cd8e0553c6b81d77094
Thabata Santos <thabata@thabatasantos.com>
2026-02-16 21:52:54 -0300

    fix static export

--- src/app/api/projects/route.js
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  const res = await fetch("https://api.github.com/users/thabata-santos/repos");
-
-  const data = await res.json();
-
-  return Response.json(data);
-}
